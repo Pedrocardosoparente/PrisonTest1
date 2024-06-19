4c42b8a4fa6b34539b9ae0f0bda3a52cf34bdf20
Aluno <Aluno@10.85.198.36>
2024-06-19 16:41:27 -0300

    PRISON

---
+++ src/Prisoner.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Aluno
+ */
+class Prisoner {
+
+    Prisoner(String bubba, double par, int par1) {
+    }
+
+    String getSentence() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    String getHeight() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/prisontest1/PrisonTest1.java1
@@ -0,0 +1,19 @@
+public class PrisonTest1 {
+    public static void main(String[] args) {
+        // Instanciando os prisioneiros
+        Prisoner twitch = new Prisoner("Twitch", 1.73, 3);
+        Prisoner bubba = new Prisoner("Bubba", 2.08, 4);
+        
+        // Imprimindo informações dos prisioneiros
+        System.out.println("Informações do prisioneiro Twitch:");
+        System.out.println("Nome: " + twitch.getName());
+        System.out.println("Altura: " + twitch.getHeight() + " m");
+        System.out.println("Sentença: " + twitch.getSentence() + " anos");
+        
+        System.out.println("\nInformações do prisioneiro Bubba:");
+        System.out.println("Nome: " + bubba.getName());
+        System.out.println("Altura: " + bubba.getHeight() + " m");
+        System.out.println("Sentença: " + bubba.getSentence() + " anos");
+    }
+}
+
